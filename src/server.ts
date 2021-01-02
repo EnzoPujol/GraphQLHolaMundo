cae47528c699981a52b35a43c68c691f10a28c5f
EnzoPujol <pujolenzo@gmail.com>
2021-01-02 16:00:32 -0300

    Inicializar repositorio

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import compression from "compression";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors);
+
+app.use(compression());
+
+app.get('/', (req, res) => {
+    res.send('Bienvenido');
+});
+
+
+const PORT = 5600;
+app.listen(
+    { port : PORT},
+    () => console.log(`hola mundo api graphql http://localhost:${PORT}`)
+);
