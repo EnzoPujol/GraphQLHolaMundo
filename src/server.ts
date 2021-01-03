29e2ae11954eeb2f8246b4069e3627c78e5fcd3a
EnzoPujol <pujolenzo@gmail.com>
2021-01-02 22:17:09 -0300

    Agregadas queries

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import compression from "compression";
 import cors from "cors";
+import { IResolvers, makeExecutableSchema } from "graphql-tools";
+import { GraphQLSchema } from "graphql";
+import { graphqlHTTP } from "express-graphql";
 
 const app = express();
 
@@ -8,13 +11,42 @@ app.use(cors());
 
 app.use(compression());
 
-app.get('/', (req, res) => {
-    res.send('Bienvenido');
+
+const typeDefs = `
+    type Query {
+        hola: String!
+        holaConNombre(nombre: String!): String
+        holaAlCursoGraphQL: String!
+    }
+`
+
+const resolvers : IResolvers = {
+    Query: {
+        hola(): string{
+            return 'Hola Mundo!';
+        },
+        holaConNombre(__: void, { nombre } ): string{
+            return `Hola Mundo, y hola ${nombre }`;
+        },
+        holaAlCursoGraphQL(): string{
+            return 'HolaMundo, estamos laburando con GraphQL';
+        }
+    }
+}
+
+const schema: GraphQLSchema = makeExecutableSchema({
+    typeDefs,
+    resolvers
 });
+
+app.use('/', graphqlHTTP({
+    schema,
+    graphiql: true
+}));
 
 
 const PORT = 5400;
 app.listen(
     { port : PORT},
-    () => console.log(`hola mundo api graphql http://localhost:${PORT}`)
+    () => console.log(`hola mundo api graphql http://localhost:${PORT}/graphql`)
 );
